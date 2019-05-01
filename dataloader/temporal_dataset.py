af5447a4d4a5757da4ae512ad030860f86929bbc
joaobelo <joaobelo92@gmail.com>
2019-05-01 11:30:05 +0200

    Temporal dataset implemented.

--- dataloader/temporal_dataset.py
+++ dataloader/temporal_dataset.py
@@ -10,7 +10,8 @@ import torchvision.transforms as transforms
 def pil_loader(path):
     with open(path, 'rb') as f:
         img = Image.open(f)
-        return img.convert('L')
+        img = img.convert('RGB')
+        return img
 
 
 class TemporalDataset(Dataset):
@@ -25,9 +26,8 @@ class TemporalDataset(Dataset):
         self.temporal_chunks = temporal_chunks
         self.time_between_frames = time_between_frames
         self.frames_temporal_flow = frames_temporal_flow
-
-        pil_loader(os.path.join(self.root_dir, 'tvl1_flow', 'u', 'v_ApplyEyeMakeup_g07_c02',
-                                                  'frame{:06d}.jpg'.format(20))).show()
+        self.image_width = 224
+        self.image_height = 224
 
     def __len__(self):
         return len(self.classes)
@@ -47,34 +47,41 @@ class TemporalDataset(Dataset):
         return data, item['label']-1
 
     def __load_optical_images(self, filename, frames):
+        data = torch.empty(self.frames_temporal_flow * 2 * self.temporal_chunks, self.image_width,
+                           self.image_height)
         offset = self.frames_temporal_flow // 2
-        for idx1, f in enumerate(frames):
+        i = 0
+        for f in frames:
             r = list(range(f - offset * self.time_between_frames, f, self.time_between_frames))
             r += list(range(f + 1, f + offset * self.time_between_frames + 1, self.time_between_frames))
-            for idx2, image_number in enumerate(r):
-                print(f, image_number)
+            for image_number in r:
                 image_u = pil_loader(os.path.join(self.root_dir, 'tvl1_flow', 'u', filename,
                                                   'frame{:06d}.jpg'.format(image_number)))
                 image_v = pil_loader(os.path.join(self.root_dir, 'tvl1_flow', 'v', filename,
                                                   'frame{:06d}.jpg'.format(image_number)))
                 if self.transform:
                     image_u = self.transform(image_u)
-
                     image_v = self.transform(image_v)
-                data = image_u if idx1 is 0 and idx2 is 0 else torch.cat((data, image_u))
-                data = torch.cat((data, image_v))
+                data[i, :, :] = image_u[0, :, :]
+                i += 1
+                data[i, :, :] = image_v[0, :, :]
+                i += 1
         return data
 
 
-dataset = TemporalDataset('/home/joao/Datasets/ucf101/', 'trainlist01.csv', transform=transforms.ToTensor())
-i, l = dataset.__getitem__(5)
-print(i.size())
-for x in i[0:4]:
-    print(i)
-    i = x.numpy().T
-
-    image = Image.fromarray(i, mode='L')
-    image.show()
+# t = transforms.Compose([
+#     transforms.CenterCrop(224),
+#     transforms.ToTensor()
+# ])
+# dataset = TemporalDataset('/Users/joaobelo/Datasets/ucf101/', 'trainlist01.csv', transform=t)
+# i, l = dataset.__getitem__(5)
+# print(i.size())
+# for x in i[0:4]:
+#     print(x)
+#     i = (x.numpy() * 255).astype(np.uint8)
+#
+#     image = Image.fromarray(i, mode='L')
+#     image.show()
 
 
 
