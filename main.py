24ccc380f8215e0d3a3b6aa36fff613c5e7fa214
Joao Belo <joaobelo92@gmail.com>
2019-05-14 20:01:48 +0200

    Initial training results + checkpoints

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ def main_worker(args):
             'state_dict': model.state_dict(),
             'best_acc1': best_acc1,
             'optimizer': optimizer.state_dict()
-        }, is_best)
+        }, is_best, args.arch)
 
 
 def train(train_loader, model, criterion, optimizer, epoch, args):
